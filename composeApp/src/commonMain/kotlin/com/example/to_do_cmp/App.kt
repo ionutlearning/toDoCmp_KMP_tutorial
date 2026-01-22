b72c92e4350e694da05879ff7a13a9a94ed419ff
ionutlearning <faurionnut@gmail.com>
2026-01-22 12:17:29 +0200

    theme

--- composeApp/src/commonMain/kotlin/com/example/to_do_cmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/to_do_cmp/App.kt
@@ -1,14 +1,19 @@
 package com.example.to_do_cmp
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.example.to_do_cmp.navigation.NavGraph
+import com.example.to_do_cmp.util.theme.darkScheme
+import com.example.to_do_cmp.util.theme.lightScheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    val colorScheme = if (isSystemInDarkTheme()) darkScheme else lightScheme
+
+    MaterialTheme(colorScheme) {
         NavGraph()
     }
 }
