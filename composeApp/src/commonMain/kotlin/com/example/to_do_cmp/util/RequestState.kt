eb7419a3b39fc4351150d1b436674e605046d7f5
ionutlearning <faurionnut@gmail.com>
2026-01-22 15:11:24 +0200

    request state and display result

---
+++ composeApp/src/commonMain/kotlin/com/example/to_do_cmp/util/RequestState.kt
@@ -0,0 +1,81 @@
+package com.example.to_do_cmp.util
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+sealed class RequestState<out T> {
+    data object Idle : RequestState<Nothing>()
+    data object Loading : RequestState<Nothing>()
+    data class Success<out T>(val data: T) : RequestState<T>()
+    data class Error(val message: String) : RequestState<Nothing>()
+
+    fun isLoading(): Boolean = this is Loading
+    private fun isSuccess(): Boolean = this is Success
+    private fun isError(): Boolean = this is Error
+
+    fun getSuccessData() = (this as Success).data
+    fun getSuccessDataOrNull() = if (this.isSuccess()) getSuccessData() else null
+
+    fun getErrorMessage() = (this as Error).message
+    fun getErrorMessageOrNull() = if (this.isError()) getErrorMessage() else null
+}
+
+@Composable
+fun <T> RequestState<T>.DisplayResult(
+    modifier: Modifier = Modifier,
+    onIdle: (@Composable () -> Unit)? = null,
+    onLoading: (@Composable () -> Unit)? = null,
+    onError: (@Composable (String) -> Unit)? = null,
+    onSuccess: @Composable (T) -> Unit,
+    backgroundColor: Color? = null,
+) {
+    val transitionSpec: ContentTransform =
+        scaleIn(tween(durationMillis = 400)) + fadeIn(tween(durationMillis = 800)) togetherWith scaleOut(
+            tween(durationMillis = 400)
+        ) + fadeOut(
+            tween(durationMillis = 800)
+        )
+    AnimatedContent(
+        modifier = modifier
+            .background(color = backgroundColor ?: Color.Unspecified),
+        targetState = this,
+        transitionSpec = { transitionSpec },
+        label = "Content Animation"
+    ) { state ->
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            when (state) {
+                is RequestState.Idle -> {
+                    onIdle?.invoke()
+                }
+
+                is RequestState.Loading -> {
+                    onLoading?.invoke()
+                }
+
+                is RequestState.Error -> {
+                    onError?.invoke(state.getErrorMessage())
+                }
+
+                is RequestState.Success -> {
+                    onSuccess(state.getSuccessData())
+                }
+            }
+        }
+    }
+}
