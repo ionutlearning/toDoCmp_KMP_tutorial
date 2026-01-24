03988cdfc6cb8f10a284f69b36f77355606fea08
ionutlearning <faurionnut@gmail.com>
2026-01-24 13:41:16 +0200

    landscape pane - nav 3 android only

--- composeApp/src/androidMain/kotlin/com/example/to_do_cmp/navigation/NavGraph.android.kt
+++ composeApp/src/androidMain/kotlin/com/example/to_do_cmp/navigation/NavGraph.android.kt
@@ -1,30 +1,62 @@
 package com.example.to_do_cmp.navigation
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.layout.calculatePaneScaffoldDirective
+import androidx.compose.material3.adaptive.navigation3.ListDetailSceneStrategy
+import androidx.compose.material3.adaptive.navigation3.rememberListDetailSceneStrategy
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
+import com.example.to_do_cmp.presentation.component.InfoCard
 import com.example.to_do_cmp.presentation.screen.home.HomeScreen
 import com.example.to_do_cmp.presentation.screen.task.TaskScreen
+import com.example.to_do_cmp.util.Resource
 import org.koin.compose.koinInject
 
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
 @Composable
 actual fun NavGraph() {
     // inject with koin
     val navigator = koinInject<Navigator>()
+    val windowAdaptiveInfo = currentWindowAdaptiveInfo()
+    val directive = remember(key1 = windowAdaptiveInfo) {
+        calculatePaneScaffoldDirective(windowAdaptiveInfo).copy(
+            horizontalPartitionSpacerSize = 0.dp
+        )
+    }
+    val listDetailSceneStrategy = rememberListDetailSceneStrategy<Any>(directive = directive)
 
     NavDisplay(
         backStack = navigator.backStack,
+        sceneStrategy = listDetailSceneStrategy,
         onBack = { navigator.goBack() },
         entryProvider = entryProvider {
-            entry<Screen.Home> {
+            entry<Screen.Home>(
+                metadata = ListDetailSceneStrategy.listPane(
+                    detailPlaceholder = {
+                        InfoCard(
+                            lightModeIcon = Resource.Image.PAINTING_LIGHT,
+                            darkModeIcon = Resource.Image.PAINTING_DARK,
+                            message = "Select a task or create a new one",
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant
+                        )
+                    }
+                )
+            ) {
                 HomeScreen(
                     navigateToTask = { taskId ->
-                        navigator.navigateTo(Screen.Task(taskId))
+                        navigator.navigateToTask(taskId)
                     }
                 )
             }
 
-            entry<Screen.Task> {
+            entry<Screen.Task>(
+                metadata = ListDetailSceneStrategy.detailPane()
+            ) {
                 TaskScreen(
                     id = it.id,
                     navigateBack = { navigator.goBack() }
--- composeApp/src/commonMain/kotlin/com/example/to_do_cmp/navigation/Navigator.kt
+++ composeApp/src/commonMain/kotlin/com/example/to_do_cmp/navigation/Navigator.kt
@@ -7,8 +7,12 @@ class Navigator {
 
     val backStack: SnapshotStateList<Screen> = mutableStateListOf(Screen.Home)
 
-    fun navigateTo(screen: Screen) {
-        backStack.add(screen)
+    fun navigateToTask(taskId: String? = null) {
+        if (backStack.lastOrNull() is Screen.Task) {
+            backStack[backStack.lastIndex] = Screen.Task(id = taskId)
+        } else {
+            backStack.add(Screen.Task(id = taskId))
+        }
     }
 
     fun goBack() {
